3186f9dfb44c6839fa09a48f1f00b51441cf653a
Andreas Löscher <andreas.loscher@it.uu.se>
2014-02-21 19:56:06 +0100

    erlydtl as a dependency and fix in the build system

--- priv/nifty_compile_templates.erl
+++ priv/nifty_compile_templates.erl
@@ -7,28 +7,28 @@ main(_) ->
 	ErlTemplate = erlang:list_to_atom("nifty_erl_template"),
 	AppTemplate = erlang:list_to_atom("nifty_app_template"),
 	ConfigTemplate = erlang:list_to_atom("nifty_config_template"),
-	ok = erlydtl:compile(
+	{ok, _} = erlydtl:compile(
 		"templates/cmodule.tpl",
 		CTemplate,
 		[{out_dir, "ebin"},
 		{force_recompile, true},
 		{custom_tags_modules, [nifty_tags]},
 		{custom_filters_modules, [nifty_filters]}]),
-	ok = erlydtl:compile(
+	{ok, _} = erlydtl:compile(
 		"templates/emodule.tpl",
 		ErlTemplate,
 		[{out_dir, "ebin"},
 		{force_recompile, true},
 		{custom_tags_modules, [nifty_tags]},
 		{custom_filters_modules, [nifty_filters]}]),
-	ok = erlydtl:compile(
+	{ok, _} = erlydtl:compile(
 		"templates/app.tpl",
 		AppTemplate,
 		[{out_dir, "ebin"},
 		{force_recompile, true},
 		{custom_tags_modules, [nifty_tags]},
 		{custom_filters_modules, [nifty_filters]}]),
-	ok = erlydtl:compile(
+	{ok, _} = erlydtl:compile(
 		"templates/config.tpl",
 		ConfigTemplate,
 		[{out_dir, "ebin"},
