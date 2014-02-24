6b39210261d7b8df34f4c5882581abc8fcde7513
Andreas Löscher <andreas.loscher@it.uu.se>
2014-02-24 11:58:44 +0100

    taged different erlydtl version

--- tests/contiki_list/proper_list.erl
+++ tests/contiki_list/proper_list.erl
@@ -1,14 +1,14 @@
 -module(proper_list).
 -export([
-	test/0,
-	llist/1,
-	item/0,
-	create_list/0,
-	next_state/3,
-	initial_state/0,
-	command/1,
-	precondition/2,
-	postcondition/3
+	 test/0,
+	 llist/1,
+	 item/0,
+	 create_list/0,
+	 next_state/3,
+	 initial_state/0,
+	 command/1,
+	 precondition/2,
+	 postcondition/3
 	]).
 
 -include_lib("proper/include/proper.hrl").
@@ -16,80 +16,78 @@
 -type llist() :: {integer(), string()}.
 -record(state, {ls :: [llist()]}).
 
-% compiler
+%% compiler
 setup() ->
-	nifty_compiler:compile("./list.h", "c_list", 
-		[{port_specs,
-				[{
+    nifty_compiler:compile("./list.h", "c_list", 
+			   [{port_specs,
+			     [{
 						".*",
 						"priv/c_list_nif.so",	
 						["/home/thegeorge/sources/contiki/core/lib/list.c"]
 				}]
 		}]).
 
-% generator
+%% generator
 item() ->
 	?LET(D, integer(), c_list:record_to_erlptr({list_item, null, D})).
 
-% helper function
+%% helper function
 create_list() ->
-	LIST = nifty:as_type(nifty:pointer(), c_list, "list_t"),
-	c_list:list_init(LIST),
-	LIST.
+    LIST = nifty:as_type(nifty:pointer(), c_list, "list_t"),
+    c_list:list_init(LIST),
+    LIST.
 
 llist(#state{ls = Lists}) ->
-	elements(Lists).
+    elements(Lists).
 
 
 initial_state() ->
-	#state{ls=[]}.
+    #state{ls=[]}.
 
 command(S) ->
-	Lists = (S#state.ls =/= []),
-	oneof(
-		[{call, proper_list, create_list, []}] ++
-		[{call, c_list, list_add, [llist(S), item()]} || Lists] ++
-		[{call, c_list, list_pop, [llist(S)]} || Lists]
-		).
+    NonEmpty = (S#state.ls =/= []),
+    oneof([{call, ?MODULE, create_list, []}] ++
+	  [{call, c_list, list_add, [llist(S), item()]} || NonEmpty] ++
+	  [{call, c_list, list_pop, [llist(S)]} || NonEmpty]
+     ).
 
 next_state(S, V, {call, _, create_list, _}) ->
-	S#state{ls=[V|S#state.ls]};
-next_state(S,_,_) ->
-	S.
+    S#state{ls=[V|S#state.ls]};
+next_state(S, _, _) ->
+    S.
 
-
-precondition(_,_) -> true.
+precondition(_, _) -> true.
 
 postcondition(_, {call, _, list_pop, _}, Result) ->
-	case Result of	
-		{0, _} -> true;
-		Pointer ->
- 			{list_item, _ , _ } = nifty:dereference(nifty:as_type(Pointer, c_list, "struct list_item *")),
-			nifty:free(Pointer),
-			true
-	end;
-postcondition(_,_,Result) ->
-	case Result of	
-		ok -> true;
-		{0, _} -> true;
-		Pointer ->
-			{list_item, _ , _ } = nifty:dereference(nifty:as_type(Pointer, c_list, "struct list_item *")),
-			true
-	end.
+    case Result of	
+	{0, _} -> true;
+	Pointer ->
+	    {list_item, _ , _ } = nifty:dereference(nifty:as_type(Pointer, c_list, "struct list_item *")),
+	    nifty:free(Pointer),
+	    true
+    end;
+postcondition(_,_, Result) ->
+    case Result of	
+	ok -> true;
+	{0, _} -> true;
+	Pointer ->
+	    {list_item, _ , _ } = nifty:dereference(nifty:as_type(Pointer, c_list, "struct list_item *")),
+	    true
+    end.
 
 prop_list_works_fine() ->
-	begin
-		setup(),
-		?FORALL(Cmds, commands(?MODULE),
-			?TRAPEXIT(
-				begin
-					{History,State,Result} = run_commands(?MODULE, Cmds),
-					true,
-					?WHENFAIL(io:format("History: ~p\nState: ~p\nResult: ~p\n",
-                                	        [History,State,Result]),
-	                              Result =:= ok)
-				end))
-	end.
+    begin
+	setup(),
+	?FORALL(Cmds, commands(?MODULE),
+		?TRAPEXIT(
+		   begin
+		       {History,State,Result} = run_commands(?MODULE, Cmds),
+		       true,
+		       ?WHENFAIL(io:format("History: ~p\nState: ~p\nResult: ~p\n",
+					   [History,State,Result]),
+				 Result =:= ok)
+		   end))
+    end.
 
 test() ->
-	proper:quickcheck(?MODULE:prop_list_works_fine(), 1000).
+    proper:quickcheck(prop_list_works_fine(), 1000).
