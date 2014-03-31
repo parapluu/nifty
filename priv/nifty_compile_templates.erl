3d14f54100a771846d7117e19cb6e3433728f3ef
Andreas Löscher <andreas.loscher@it.uu.se>
2014-03-31 17:12:12 +0200

    better code

--- priv/nifty_compile_templates.erl
+++ priv/nifty_compile_templates.erl
@@ -2,18 +2,14 @@
 -export([main/1]).
 
 main(_) ->
-    io:format("Compiling Templates~n"),
-    CTemplate = list_to_atom("nifty_c_template"),
-    ErlTemplate = list_to_atom("nifty_erl_template"),
-    AppTemplate = list_to_atom("nifty_app_template"),
-    ConfigTemplate = list_to_atom("nifty_config_template"),
-    Options = [{out_dir, "ebin"},
-	       {force_recompile, true},
-	       {custom_tags_modules, [nifty_tags]},
+    ok = io:format("Compiling Templates~n"),
+    Options = [{out_dir, "ebin"}, 
+	       {force_recompile, true}, 
+	       {custom_tags_modules, [nifty_tags]}, 
 	       {custom_filters_modules, [nifty_filters]}, 
 	       {compiler_options, [debug_info]}],
-    {ok, _} = erlydtl:compile("templates/cmodule.tpl", CTemplate, Options),
-    {ok, _} = erlydtl:compile("templates/emodule.tpl", ErlTemplate, Options),
-    {ok, _} = erlydtl:compile("templates/app.tpl", AppTemplate, Options),
-    {ok, _} = erlydtl:compile("templates/config.tpl", ConfigTemplate, Options).
+    {ok, nifty_c_template} = erlydtl:compile("templates/cmodule.tpl", nifty_c_template, Options),
+    {ok, nifty_erl_template} = erlydtl:compile("templates/emodule.tpl", nifty_erl_template, Options),
+    {ok, nifty_app_template} = erlydtl:compile("templates/app.tpl", nifty_app_template, Options),
+    {ok, nifty_config_template} = erlydtl:compile("templates/config.tpl", nifty_config_template, Options).
 
