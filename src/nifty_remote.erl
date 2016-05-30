e2e0690cd922a77869a4a7823248c91dc1d2968c
Andreas Löscher <andreas.loscher@it.uu.se>
2016-05-30 16:52:44 +0200

    updated specs

--- src/nifty_remote.erl
+++ src/nifty_remote.erl
@@ -28,16 +28,27 @@
          as_type/2,
          size_of/1,
          %% memory allocation
+         malloc/1,
          mem_write/1,
          mem_write/2,
          mem_read/2,
          mem_alloc/1,
+         mem_copy/3,
          free/1,
          %% configuration
          get_config/0,
          get_env/0,
          %% builtin types
-         get_types/0
+         get_types/0,
+         %% array
+         array_new/2,
+         array_ith/2,
+         array_element/2,
+         array_set/3,
+         array_to_list/2,
+         list_to_array/2,
+         %% enums
+         enum_value/2
         ]).
 
 -spec start() -> ok.
@@ -48,74 +59,110 @@ start() ->
 stop() ->
   nifty_remotecall:stop().
 
--spec compile (term(),term(),term()) -> term().
+-spec compile(string(), module(), nifty:options()) -> nifty:comp_ret() | {error, node_down}.
 compile(X0,X1,X2) ->
   nifty_remotecall:call_remote(nifty, compile, [X0,X1,X2]).
 
--spec list_to_cstr (term()) -> term().
+-spec list_to_cstr(string()) -> nifty:ptr() | {error, node_down}.
 list_to_cstr(X0) ->
   nifty_remotecall:call_remote(nifty, list_to_cstr, [X0]).
 
--spec cstr_to_list (term()) -> term().
+-spec cstr_to_list(nifty:ptr()) -> string() | {error, node_down}.
 cstr_to_list(X0) ->
   nifty_remotecall:call_remote(nifty, cstr_to_list, [X0]).
 
--spec dereference (term()) -> term().
+-spec dereference(nifty:ptr()) -> nifty:cvalue() | {error, node_down}.
 dereference(X0) ->
   nifty_remotecall:call_remote(nifty, dereference, [X0]).
 
--spec pointer () -> term().
+-spec pointer() -> nifty:ptr() | {error, node_down}.
 pointer() ->
   nifty_remotecall:call_remote(nifty, pointer, []).
 
--spec pointer (term()) -> term().
+-spec pointer(nonempty_string()) -> nifty:ptr() | undef | {error, node_down}.
 pointer(X0) ->
   nifty_remotecall:call_remote(nifty, pointer, [X0]).
 
--spec pointer_of (term(),term()) -> term().
+-spec pointer_of(term(), string()) -> nifty:ptr() | undef | {error, node_down}.
 pointer_of(X0,X1) ->
   nifty_remotecall:call_remote(nifty, pointer_of, [X0,X1]).
 
--spec pointer_of (term()) -> term().
+-spec pointer_of(nifty:ptr()) -> nifty:ptr() | undef | {error, node_down}.
 pointer_of(X0) ->
   nifty_remotecall:call_remote(nifty, pointer_of, [X0]).
 
--spec as_type (term(),term()) -> term().
+-spec as_type(nifty:ptr(), nonempty_string()) -> nifty:ptr() | undef | {error, node_down}.
 as_type(X0,X1) ->
   nifty_remotecall:call_remote(nifty, as_type, [X0,X1]).
 
--spec size_of (term()) -> term().
+-spec size_of(nonempty_string()) -> integer() | undef | {error, node_down}.
 size_of(X0) ->
   nifty_remotecall:call_remote(nifty, size_of, [X0]).
 
--spec mem_write (term()) -> term().
+-spec mem_write(binary() | list()) -> nifty:ptr() | {error, node_down}.
 mem_write(X0) ->
   nifty_remotecall:call_remote(nifty, mem_write, [X0]).
 
--spec mem_write (term(),term()) -> term().
+-spec mem_write(nifty:ptr(), binary() | list()) -> nifty:ptr() | {error, node_down}.
 mem_write(X0,X1) ->
   nifty_remotecall:call_remote(nifty, mem_write, [X0,X1]).
 
--spec mem_read (term(),term()) -> term().
+-spec mem_read (term(),term()) -> term() | {error, node_down}.
 mem_read(X0,X1) ->
   nifty_remotecall:call_remote(nifty, mem_read, [X0,X1]).
 
--spec mem_alloc (term()) -> term().
+-spec mem_alloc(non_neg_integer()) -> nifty:ptr() | {error, node_down}.
 mem_alloc(X0) ->
   nifty_remotecall:call_remote(nifty, mem_alloc, [X0]).
 
--spec free (term()) -> term().
+-spec free(nifty:ptr()) -> 'ok' | {error, node_down}.
 free(X0) ->
   nifty_remotecall:call_remote(nifty, free, [X0]).
 
--spec get_config () -> term().
+-spec get_config() -> proplists:proplist() | {error, node_down}.
 get_config() ->
   nifty_remotecall:call_remote(nifty, get_config, []).
 
--spec get_env () -> term().
+-spec get_env() -> {integer(), nonempty_string()} | {error, node_down}.
 get_env() ->
   nifty_remotecall:call_remote(nifty, get_env, []).
 
--spec get_types () -> term().
+-spec get_types() -> dict:dict() | {error, node_down}.
 get_types() ->
   nifty_remotecall:call_remote(nifty, get_types, []).
+
+-spec enum_value(atom(), nonempty_string() | atom()) -> integer() | undef.
+enum_value(X0, X1) ->
+  nifty_remotecall:call_remote(nifty, enum_value, [X0, X1]).
+
+-spec mem_copy(nifty:ptr(), nifty:ptr(), non_neg_integer()) -> ok.
+mem_copy(X0, X1, X2) ->
+  nifty_remotecall:call_remote(nifty, mem_copy, [X0, X1, X2]).
+
+-spec array_new(nonempty_string(), non_neg_integer()) -> nifty:ptr().
+array_new(X0, X1) ->
+  nifty_remotecall:call_remote(nifty, array_new, [X0, X1]).
+
+-spec array_ith(nifty:ptr(), integer()) -> nifty:ptr().
+array_ith(X0, X1) ->
+  nifty_remotecall:call_remote(nifty, array_ith, [X0, X1]).
+
+-spec array_element(nifty:ptr(), integer()) -> nifty:cvalue().
+array_element(X0, X1) ->
+  nifty_remotecall:call_remote(nifty, array_element, [X0, X1]).
+
+-spec array_set(nifty:ptr(), term(), integer()) -> ok.
+array_set(X0, X1, X2) ->
+  nifty_remotecall:call_remote(nifty, array_set, [X0, X1, X2]).
+
+-spec array_to_list(nifty:ptr(), non_neg_integer()) -> [nifty:cvalue()].
+array_to_list(X0, X1) ->
+  nifty_remotecall:call_remote(nifty, array_to_list, [X0, X1]).
+
+-spec list_to_array(list(), nonempty_string()) -> nifty:ptr().
+list_to_array(X0, X1) ->
+  nifty_remotecall:call_remote(nifty, list_to_array, [X0, X1]).
+
+-spec malloc(non_neg_integer()) -> nifty:ptr().
+malloc(X0) ->
+  mem_alloc(X0).
